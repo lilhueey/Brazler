69dd5681bcc8ac0fb2c5145a38056b5299678b43
huey <quintanajvitorhh@gmail.com>
2024-10-21 23:00:09 -0300

    Add files via upload

---
+++ js/list.js
@@ -0,0 +1,30 @@
+const d = {
+  "list": [
+    /*=================================================================================*/
+        {
+            "vids": [
+              {
+                "user": "SpeedyYT_2449",
+                "link": "https://www.youtube.com/watch?v=hqokP172p9k",
+                "percent": 100,
+                "hz": "240hz"
+              },
+            ],
+            "name": "COSMIC",
+            "author": "Lells [Speedy]",
+            "more": "none",
+            "id": 110176794,
+            "pass": "Cópia livre",
+            "percentToQualify": 60,
+            "verificationVid": "https://www.youtube.com/watch?v=hqokP172p9k",
+            "key": 0
+        },
+    ],
+    "version": [
+      1,
+      0,
+      0
+    ]
+  };
+  
+const list = d.list; const version = d.version;
